39f004aee9655f82029dcbf8e9fe651fdf242eca
ben_coelho <ben_coelho@hotmail.com>
2020-07-16 21:20:34 +1000

    Tests passed

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -4,9 +4,10 @@ class Manager extends Employee {
   constructor(name, id, email, officeNumber) {
     super(name, id, email);
     this.role = "Manager";
+    this.officeNumber=officeNumber;
   }
 
-  getSchool() {
+  getOfficeNumber() {
     return this.officeNumber;
   }
 }
