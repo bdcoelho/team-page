7ad86fa4d1e11bbaae08a7d44fcadeb46791dd6c
bdcoelho <ben_coelho@hotmail.com>
2020-07-20 12:00:44 +1000

    Working

--- Develop/app.js
+++ Develop/app.js
@@ -5,11 +5,14 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 let answers = {};
 let employeeArray = [];
